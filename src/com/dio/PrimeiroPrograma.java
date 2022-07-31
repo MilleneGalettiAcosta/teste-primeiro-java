afd31ae0c1311754f40cba9a8d3a2b2617c8da79
Millene Galetti <mikagaletti1@gmail.com>
2022-07-31 20:09:15 -0300

    Commit  test

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -7,5 +7,19 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro = new Livro("O nome do livro", 300);
+        System.out.println(livro);
     }
+}
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
 }
