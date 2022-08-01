6485606bda4190ee83f395466bfc6d46ec95af22
Millene Galetti <mikagaletti1@gmail.com>
2022-08-01 14:59:16 -0300

    Commit  métodos - sobrecarga - áreas aplicando retornos

--- src/com/dio/metodos/Quadrilatero.java
+++ src/com/dio/metodos/Quadrilatero.java
@@ -2,15 +2,15 @@ package com.dio.metodos;
 
 public class Quadrilatero {
 
-    public static void area(double lado) {
-        System.out.println("área do quadrado" + lado * lado);
+    public static double area(double lado) {
+        return lado *lado;
     }
 
-    public static void area(double lado1, double lado2) {
-        System.out.println("área do retângulo" + lado1 * lado2);
+    public static double area(double lado1, double lado2) {
+        return lado1 * lado2;
     }
 
-    public static void  area(double baseMenor, double baseMaior, double altura) {
-        System.out.println("Área do triangulo" + ((baseMenor + baseMaior) * altura) / 2);
+    public static double  area(double baseMenor, double baseMaior, double altura) {
+       return ((baseMenor + baseMaior) * altura) / 2;
     }
 }
